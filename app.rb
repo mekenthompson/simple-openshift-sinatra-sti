b6cf257778052f5246d1418dc99fe1df24db0da7
Ken Thompson <me@kenthompson.com.au>
2016-03-02 16:32:55 +1100

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-  "Hello World, I'm a developer pushing code!! Push...... 1 2 3"
+  "Hello World, I'm a developer pushing code!! Push...... 1 2 3 unsecured"
 end
